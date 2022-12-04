cb3b3e50caca03ec087f31d832c36c6671ba0a08
Fabio Cirelli <fabio.cire.156@gmail.com>
2022-12-04 09:59:08 +0100

    Added Classification Tree and Binary Logistic Regression models

---
+++ titanic_tree.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+# Read data
+train_data = pd.read_csv('.\\data\\train.csv')
+test_data = pd.read_csv('.\\data\\test.csv')
+
+# Separate dependent variable from dependent variables
+# Create dummy indicators of multinomial features
+y = train_data["Survived"]
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+# Create and fit model, make predictions
+model = DecisionTreeClassifier()
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+# Write results to csv
+output = pd.DataFrame({'PassengerId' : test_data.PassengerId, 'Survived' : predictions})
+output.to_csv('results_tree.csv', index = False)
